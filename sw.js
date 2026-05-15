c2170ca1d8fa939789d65067835f9d3c1190b06d
MKANE955 <hemmmiguez@gmail.com>
2026-05-15 20:20:41 -0300

    agrego PWA: manifest, service worker e iconos

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'zona-azul-v1';
+const ASSETS = [
+  '/zona-azul/',
+  '/zona-azul/index.html',
+  '/zona-azul/icon-192.png',
+  '/zona-azul/icon-512.png',
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
